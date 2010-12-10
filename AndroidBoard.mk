cdb4434cb7a35969d117d0deb430c1495cc91e7a
Quarx2k <agent00791@gmail.com>
2010-12-11 01:06:55 +0600

    Added wmadec and wmvdec

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -33,12 +33,14 @@ include $(BUILD_PREBUILT)
 ## OMX proprietaries
 PRODUCT_COPY_FILES += \
     device/lg/swift/proprietary/omx/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
-    device/lg/swift/proprietary/omx/libmm-omxcore.so:system/lib/libmm-omxcore.so \
     device/lg/swift/proprietary/omx/libOmxH264Dec.so:system/lib/libOmxH264Dec.so \
-    device/lg/swift/proprietary/omx/libOmxCore.so:system/lib/libOmxCore.so \
     device/lg/swift/proprietary/omx/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
-    device/lg/swift/proprietary/omx/libOmxMpeg4Dec.so:system/lib/libOmxMpeg4Dec.so 
+    device/lg/swift/proprietary/omx/libOmxMpeg4Dec.so:system/lib/libOmxMpeg4Dec.so \
+    device/lg/swift/proprietary/omx/libomx_wmadec_sharedlibrary.so:system/lib/libomx_wmadec_sharedlibrary.so \
+    device/lg/swift/proprietary/omx/libomx_wmvdec_sharedlibrary.so:system/lib/libomx_wmvdec_sharedlibrary.so 
 
+#device/lg/swift/proprietary/omx/libOmxCore.so:system/lib/libOmxCore.so \
+#device/lg/swift/proprietary/omx/libmm-omxcore.so:system/lib/libmm-omxcore.so \
 #device/lg/swift/proprietary/omx/libOmxAacDec.so:system/lib/libOmxAacDec.so \
 #device/lg/swift/proprietary/omx/llibOmxAmrDec.so:system/lib/libOmxAmrDec.so \
 #device/lg/swift/proprietary/omx/libOmxAmrEnc.so:system/lib/libOmxAmrEnc.so \
