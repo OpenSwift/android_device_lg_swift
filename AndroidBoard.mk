2eedc29a38262ca58e22d1713e08713fafc93442
Quarx2k <agent00791@gmail.com>
2010-12-12 18:56:10 +0600

    Fix copy libs

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -14,7 +14,13 @@ LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
 OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
 include $(BUILD_PREBUILT)
 
-
+include $(CLEAR_VARS)
+LOCAL_MODULE := libsurfaceflinger.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
+OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+include $(BUILD_PREBUILT)
 
 
 #PRODUCT_COPY_FILES += \
@@ -27,6 +33,7 @@ include $(BUILD_PREBUILT)
 
 ## OMX proprietaries
 PRODUCT_COPY_FILES += \
+	device/lg/swift/proprietary/libsurfaceflinger.so:obj/lib/libsurfaceflinger.so \
         device/lg/swift/proprietary/omx/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
         device/lg/swift/proprietary/omx/libOmxH264Dec.so:system/lib/libOmxH264Dec.so \
         device/lg/swift/proprietary/omx/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
@@ -39,15 +46,14 @@ PRODUCT_COPY_FILES += \
 	device/lg/swift/proprietary/omx/libOmxAmrEnc.so:system/lib/libOmxAmrEnc.so \
 	device/lg/swift/proprietary/omx/libOmxAmrRtpDec.so:system/lib/libOmxAmrRtpDec.so \
 	device/lg/swift/proprietary/omx/libOmxAmrwbDec.so:system/lib/libOmxAmrwbDec.so \
-	device/lg/swift/proprietary/omx/ibOmxEvrcDec.so:system/lib/libOmxEvrcDec.so \
+	device/lg/swift/proprietary/omx/libOmxEvrcDec.so:system/lib/libOmxEvrcDec.so \
 	device/lg/swift/proprietary/omx/libOmxEvrcEnc.so:system/lib/libOmxEvrcEnc.so \
 	device/lg/swift/proprietary/omx/libOmxAacEnc.so:system/lib/libOmxAacEnc.so \
-	device/lg/swift/proprietary/omx/llibOmxMp3Dec.so:system/lib/libOmxMp3Dec.so \
+	device/lg/swift/proprietary/omx/libOmxMp3Dec.so:system/lib/libOmxMp3Dec.so \
 	device/lg/swift/proprietary/omx/libOmxQcelp13Enc.so:system/lib/libOmxQcelp13Enc.so \
 	device/lg/swift/proprietary/omx/libOmxQcelpDec.so:system/lib/libOmxQcelpDec.so \
 	device/lg/swift/proprietary/omx/libOmxWmaDec.so:system/lib/libOmxWmaDec.so \
-	device/lg/swift/proprietary/libsurfaceflinger.so:system/lib/libsurfaceflinger.so \
-	device/lg/swift/proprietary/omx/llibOmxWmvDec.so:system/lib/libOmxWmvDec.so \
+	device/lg/swift/proprietary/omx/libOmxWmvDec.so:system/lib/libOmxWmvDec.so \
 	device/lg/swift/proprietary/omx/libaomx_mp4dec_sharedlibrary.so:system/lib/libaomx_mp4dec_sharedlibrary.so \
 	device/lg/swift/proprietary/omx/libaomx_mp3dec_sharedlibrary.so:system/lib/libaomx_mp3dec_sharedlibrary.so
 
@@ -84,8 +90,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES +=  \
     vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
 
-# device/lg/swift/proprietary/lgdrmserver:system/bin/lgdrmserver 
-# device/lg/swift/proprietary/lgesystemd:system/bin/lgesystemd 
 
 #GSM
 PROPRIETARY += lib/liblgdrmwbxml.so lib/libdll.so lib/libril-qcril-hook-oem.so  lib/libwms.so \
