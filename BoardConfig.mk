94b7cebe403495ba36971376ab528f4439abf6ea
Quarx2k <agent00791@gmail.com>
2011-01-09 01:14:22 +0600

    fix configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,38 +15,31 @@ TARGET_NO_KERNEL := true
 TARGET_NO_RADIO := true
 
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-WPA_SUPPLICANT_VERSION      := VER_0_5_X
-BOARD_WLAN_DEVICE           := bcm4325
+WPA_SUPPLICANT_VERSION      := VER_0_6_X
+BOARD_WLAN_DEVICE           := bcm4329
 WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/wireless.ko"
 WIFI_DRIVER_FW_STA_PATH     := "/system/etc/wl/rtecdc.bin"
 WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wl/rtecdc.bin nvram_path=/system/etc/wl/nvram.txt"
 WIFI_DRIVER_MODULE_NAME     := "wireless"
 
-# Use HTC USB Function Switch to enable tethering via USB
-BOARD_USE_HTC_USB_FUNCTION_SWITCH := true
-
-#BOARD_USES_ECLAIR_LIBCAMERA := true
-
 BOARD_USES_QCOM_LIBS := true
 BOARD_USES_QCOM_HARDWARE := true
-USE_CAMERA_STUB := false
-
 BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
-
 BOARD_HAVE_BLUETOOTH    := true
 BOARD_HAVE_BLUETOOTH_BCM    := true
 WITH_A2DP := true
 TINY_TOOLBOX:=true
 BOARD_HAVE_FM_RADIO := true
+BUILD_PV_VIDEO_ENCODERS := true
 BOARD_GPS_LIBRARIES :=  libloc_api
 
-BUILD_PV_VIDEO_ENCODERS := true
 
 TARGET_HARDWARE_3D := false
 USE_PV_WINDOWS_MEDIA := false
-
+USE_CAMERA_STUB := false
 TARGET_HARDWARE_3D := false
 BOARD_USES_GENERIC_AUDIO := false
 BOARD_USE_HTC_LIBSENSORS := false
 
 BOARD_EGL_CFG := device/lg/swift/egl.cfg
+
